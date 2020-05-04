e9fe45d8e59fb004d3298c4a77d66c526313fd37
cahlchang <kahlua.dane@gmail.com>
2020-05-04 23:44:38 +0900

    notify setting

--- yig/plugins/photo.py
+++ yig/plugins/photo.py
@@ -1,6 +1,7 @@
 from yig.bot import listener
 
 
-@listener("saveimg")
+@listener("SAVEIMG")
 def save_image(bot):
     print("save image test")
+    return "save image test", "#111111"
--- yig/util.py
+++ yig/util.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def post_command(message, token, data_user, channel_id, is_replace_plus=False):
     command_url = "https://slack.com/api/chat.postMessage?"
@@ -16,3 +17,22 @@ def post_command(message, token, data_user, channel_id, is_replace_plus=False):
     print(payload)
     res = requests.get(command_url, params=payload)
     print(res.url)
+
+
+def return_param_builder(response_url, user_id, return_message, color, response_type="in_channel"):
+    payload = {
+        "icon_emoji": "books",
+        "response_type": response_type,
+        "replace_original": False,
+        "headers": {},
+        "text": "<@{}>".format(user_id),
+        "attachments": json.dumps([
+            {
+                "text": return_message,
+                "type": "mrkdwn",
+                "color": color
+            }
+        ])}
+
+    res = requests.post(response_url, data=json.dumps(payload))
+    print(res.text)
