5ae72e8857c508a2c24a5e0852d3a95f3926da8e
cahlchang <kahlua.dane@gmail.com>
2020-08-05 15:22:12 +0900

    test修正

--- tests/test_lambda_function.py
+++ tests/test_lambda_function.py
@@ -5,33 +5,44 @@ import re
 import pytest
 import lambda_function as main
 from yig.plugins.group import analyze_join_command, analyze_kp_order_command
+from yig.plugins.roll import judge_1d100, split_alternative_roll_or_value, create_post_message_rolls_result, get_sanc_result, eval_roll_or_value
+from yig.plugins.user import analyze_update_command
+from yig.util import get_status_message
+from yig.config import COLOR_CRITICAL, COLOR_SUCCESS, COLOR_FAILURE, COLOR_FUMBLE
+
+
+def test_judge_1d100(target, actual, exp_msg, exp_color):
+    msg, color = judge_1d100(target, actual)
+    assert msg == exp_msg
+    assert color == exp_color
 
 
 def test_2():
     msg = "【test_user】TEST\nHP 4/5\u3000\u3000MP 4/5\u3000\u3000DEX 8\u3000\u3000SAN 10/10"
-    assert main.get_status_message("TEST",
-                                   {"name": "test_user",
-                                    "HP": 5,
-                                    "MP": 5,
-                                    "DEX": 8,
-                                    "現在SAN": 10,
-                                    "SAN": 15},
-                                   {"HP": -1,
-                                    "MP": -1}) == msg
+    assert get_status_message("TEST",
+                              {"name": "test_user",
+                               "HP": 5,
+                               "MP": 5,
+                               "DEX": 8,
+                               "現在SAN": 10,
+                               "SAN": 15},
+                              {"HP": -1,
+                               "MP": -1}) == msg
 
 
 @pytest.mark.parametrize("target, actual, exp_msg, exp_color", [
-    (1, 1, "クリティカル", main.COLOR_CRITICAL),
-    (5, 5, "クリティカル", main.COLOR_CRITICAL),
-    (20, 15, "成功", main.COLOR_SUCCESS),
-    (100, 100, "成功", main.COLOR_SUCCESS),
-    (1, 5, "失敗", main.COLOR_FAILURE),
-    (94, 95, "失敗", main.COLOR_FAILURE),
-    (95, 96, "ファンブル", main.COLOR_FUMBLE),
-    (95, 100, "ファンブル", main.COLOR_FUMBLE),
+    (1, 1, "クリティカル", COLOR_CRITICAL),
+    (5, 5, "クリティカル", COLOR_CRITICAL),
+    (20, 15, "成功", COLOR_SUCCESS),
+    (100, 100, "成功", COLOR_SUCCESS),
+    (1, 5, "失敗", COLOR_FAILURE),
+    (94, 95, "失敗", COLOR_FAILURE),
+    (95, 96, "ファンブル", COLOR_FUMBLE),
+    (95, 100, "ファンブル", COLOR_FUMBLE),
 ])
+
 def test_judge_1d100(target, actual, exp_msg, exp_color):
-    msg, color = main.judge_1d100(target, actual)
+    msg, color = judge_1d100(target, actual)
     assert msg == exp_msg
     assert color == exp_color
 
@@ -47,7 +58,7 @@ def test_judge_1d100(target, actual, exp_msg, exp_color):
     ("1/hoge", None),
 ])
 def test_split_alternative_roll_or_value(cmd, result):
-    assert main.split_alternative_roll_or_value(cmd) == result
+    assert split_alternative_roll_or_value(cmd) == result
 
 
 @pytest.mark.parametrize("text, count, min_val, max_val", [
@@ -56,7 +67,7 @@ def test_split_alternative_roll_or_value(cmd, result):
     ("1d6", 1, 0, 0),
 ])
 def test_eval_roll_or_value_for_roll(text, count, min_val, max_val):
-    results = main.eval_roll_or_value(text)
+    results = eval_roll_or_value(text)
     assert len(results) == count
     for val in results:
         assert min_val <= val
@@ -70,7 +81,7 @@ def test_eval_roll_or_value_for_roll(text, count, min_val, max_val):
     ("100a", 0),
 ])
 def test_eval_roll_or_value_for_value(text, val):
-    results = main.eval_roll_or_value(text)
+    results = eval_roll_or_value(text)
     assert len(results) == 1
     assert results[0] == val
 
@@ -85,15 +96,15 @@ def test_format_as_command(text, expected):
 
 
 @pytest.mark.parametrize("cmd, san_val, msg_matcher, color", [
-    ("SANC", 100, r"成功 【SANチェック】 \d+/100", main.COLOR_SUCCESS),
-    ("SANC", 0, r"失敗 【SANチェック】 \d+/0", main.COLOR_FAILURE),
+    ("SANC", 100, r"成功 【SANチェック】 \d+/100", COLOR_SUCCESS),
+    ("SANC", 0, r"失敗 【SANチェック】 \d+/0", COLOR_FAILURE),
     ("SANC 1/1D3", 100,
-     r"成功 【SANチェック】 \d+/100\n【減少値】 \d", main.COLOR_SUCCESS),
+     r"成功 【SANチェック】 \d+/100\n【減少値】 \d", COLOR_SUCCESS),
     ("SANC 1/1D3", 0,
-     r"失敗 【SANチェック】 \d+/0\n【減少値】 \d", main.COLOR_FAILURE),
+     r"失敗 【SANチェック】 \d+/0\n【減少値】 \d", COLOR_FAILURE),
 ])
 def test_get_sanc_result(cmd, san_val, msg_matcher, color):
-    actual_msg, actual_color = main.get_sanc_result(cmd, san_val)
+    actual_msg, actual_color = get_sanc_result(cmd, san_val)
     assert re.match(msg_matcher, actual_msg) is not None
     assert actual_color == color
 
@@ -104,8 +115,7 @@ def test_get_sanc_result(cmd, san_val, msg_matcher, color):
      "1 [plus] \n" + f"1".ljust(80) + "1 [plus] \n", 2),
 ])
 def test_create_post_message_rolls_result(text, message, detail, sum_val):
-    result_message, resule_detail, result_sum = main.create_post_message_rolls_result(
-        text)
+    result_message, resule_detail, result_sum = create_post_message_rolls_result(text)
     assert result_message == message
     assert resule_detail == detail
     assert result_sum == sum_val
@@ -117,7 +127,7 @@ def test_create_post_message_rolls_result(text, message, detail, sum_val):
     ("u HP - 5", "HP", "-", "5"),
 ])
 def test_analyze_update_command(command, exp_status_name, exp_operator, exp_arg):
-    result = main.analyze_update_command(command)
+    result = analyze_update_command(command)
     assert result
     status_name, operator, arg = result
     assert status_name == exp_status_name
@@ -130,7 +140,7 @@ def test_analyze_update_command(command, exp_status_name, exp_operator, exp_arg)
     ("u SAN^10"),
 ])
 def test_analyze_update_command_invalid(command):
-    result = main.analyze_update_command(command)
+    result = analyze_update_command(command)
     assert result is None
 
 
