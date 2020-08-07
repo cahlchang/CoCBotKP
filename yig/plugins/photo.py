c43758bc3e0e38f598e030369d703ce06e301b18
cahlchang <kahlua.dane@gmail.com>
2020-08-07 18:10:01 +0900

    util関数に外だし。key構造変更対応。

--- yig/plugins/photo.py
+++ yig/plugins/photo.py
@@ -1,40 +1,25 @@
 import requests
-import os
-import boto3
+
 
 from yig.bot import listener
-from yig.util import get_state_data, get_charaimage
+from yig.util import get_state_data, write_pc_image, get_charaimage
 import yig.config
 
 
 @listener("SAVEIMG")
 def icon_save_image(bot):
     """:art: *save slack icon*\n`/cc saveimg`"""
-    state_data = get_state_data(bot.user_id)
+    state_data = get_state_data(bot.team_id, bot.user_id)
     icon_url = bot.data_user["profile"]["image_512"]
-    response = requests.get(icon_url, stream=True)
-    content_type = response.headers["content-type"]
-    if 'image' not in content_type:
-        exception = Exception("Content-Type: " + content_type)
-        raise exception
-
-    filename = "%s%s" % (state_data["pc_id"], "_image")
-
-    s3_client = boto3.resource('s3')
-    bucket = s3_client.Bucket(yig.config.AWS_S3_BUCKET_NAME)
-
-    key_image = "%s/%s" % (bot.user_id, filename)
-    # todo 上書きを禁止する
-    bucket.upload_fileobj(response.raw, key_image)
-
+    image_path = write_pc_image(bot.team_id, bot.user_id, state_data["pc_id"], icon_url)
     return "アイコンを保存しました。", yig.config.COLOR_ATTENTION
 
 
 @listener("LOADIMG")
 def icon_load_image(bot):
     """:frame_with_picture: *load icon image*\n`/cc loadimg`"""
-    state_data = get_state_data(bot.user_id)
-    image = get_charaimage(bot.user_id, state_data["pc_id"])
+    state_data = get_state_data(bot.team_id, bot.user_id)
+    image = get_charaimage(bot.team_id, bot.user_id, state_data["pc_id"])
     param = {
         'token': bot.token,
         "channels": bot.channel_id
