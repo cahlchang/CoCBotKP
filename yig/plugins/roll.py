aa138c9d7d7c314e98f4381b8f05ebfbb41b5b60
cahlchang <kahlua.dane@gmail.com>
2020-08-14 16:10:07 +0900

    hideコマンドを通常rollと合わせることによって整理

--- yig/plugins/roll.py
+++ yig/plugins/roll.py
@@ -44,51 +44,36 @@ def hide_roll(bot):
     def post_hide(user_id):
         post_url = 'https://slack.com/api/chat.postMessage'
         dict_state = get_state_data(bot.team_id, bot.user_id)
-        param = get_user_param(bot.team_id, bot.user_id, dict_state["pc_id"])
-        color_hide = "gray"
+        user_param = get_user_param(bot.team_id, bot.user_id, dict_state["pc_id"])
         channel = '@' + dict_state["kp_id"]
         key = bot.key
         text = ""
         m = re.match(r"HIDE\s(.*?)(\+|\-|\*|\/)?(\d{,})?$", key)
-        if m is None:
-            text = "role not found"
-            post_message = f"技能名が解釈できません。\n{key}"
-        elif m.group(1) and m.group(1) not in param:
-            text = "role miss"
-            name_role = m.group(1)
-            post_message = f"この技能は所持していません。\n{key}"
+        if m.group(1) not in user_param.keys():
+            text = f"<@{user_id}> try talk"
+            post_message = f"{key}"
+            color = "gray"
         else:
-            name_role = m.group(1)
-            n_targ = 0
-            msg_rev = "+0"
-            if type(param[name_role]) == list:
-                n_targ = int(param[name_role][5])
+            hide_message = "".join(['' if v is None else v for v in m.groups()])
+            roll, operant, num_arg = analysis_roll_and_calculation(hide_message)
+
+            alias_roll = {"こぶし": "こぶし（パンチ）"}
+            if roll in alias_roll.keys():
+                roll = alias_roll[roll]
+
+            data = user_param[roll]
+
+            num_rand = int(random.randint(1, 100))
+            if roll.upper() in yig.config.LST_USER_STATUS_NAME:
+                num = int(data)
             else:
-                n_targ = int(param[name_role])
+                num = int(data[-1])
 
-            msg_disp = n_targ
+            num_targ = calculation(num, operant, num_arg)
+            result, color = judge_1d100(num_targ, num_rand)
 
-            if name_role in dict_state:
-                n_targ += dict_state[name_role]
-            if m.group(2) is not None:
-                if m.group(2) == "+":
-                    n_targ += int(m.group(3))
-                    msg_rev = "+" + str(m.group(3))
-                elif m.group(2) == "-":
-                    n_targ -= int(m.group(3))
-                    msg_rev = "-" + str(m.group(3))
-                elif m.group(2) == "*":
-                    n_targ *= int(m.group(3))
-                    msg_rev = "*" + str(m.group(3))
-                elif m.group(2) == "/":
-                    n_targ /= int(m.group(3))
-                    msg_rev = "/" + str(m.group(3))
-
-            num = int(random.randint(1, 100))
-
-            str_result, color_hide = judge_1d100(int(n_targ), num)
-            post_message = f"{str_result} 【{name_role}】 {num}/{n_targ} ({msg_disp}{msg_rev})"
-            text = f"<@{user_id}> try {name_role}"
+            text = f"<@{user_id}> try {roll}"
+            post_message = f"{result} 【{roll}】 {num_rand}/{num_targ} ({num}{operant}{num_arg})"
 
         payload = {
             'text': text,
@@ -96,7 +81,7 @@ def hide_roll(bot):
                 {
                     "text": post_message,
                     "type": "mrkdwn",
-                    "color": "gray"
+                    "color": color
                 }
             ])
         }
@@ -110,7 +95,7 @@ def hide_roll(bot):
         future_hide.result()
 
     return_payload = {
-        "text": "結果は公開されず、KPが描写だけ行います",
+        "text": "",
         "attachments": json.dumps([
             {
                 "text": "【シークレットダイス】？？？",
@@ -128,7 +113,6 @@ def roll_skill(bot):
     roll, operant, num_arg = analysis_roll_and_calculation(bot.message)
 
     alias_roll = {"こぶし": "こぶし（パンチ）"}
-
     if roll in alias_roll.keys():
         roll = alias_roll[roll]
 
