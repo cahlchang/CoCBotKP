d807a551d183743b31e9e3f8dc9dc499db698b3d
cahlchang <kahlua.dane@gmail.com>
2020-08-26 19:00:05 +0900

    パラメーター画像作成、画像保存、パス取得関数を追加

--- yig/util/view.py
+++ yig/util/view.py
@@ -2,8 +2,8 @@ import requests
 import json
 import boto3
 import os
-import copy
-from PIL import Image
+import math
+from PIL import Image, ImageDraw, ImageFont
 
 import yig.config
 
@@ -12,7 +12,7 @@ def write_pc_image(team_id, user_id, pc_id, url):
     """Convert the image to a png image and write it in S3."""
     image_origin_path = f"/tmp/origin_image"
     image_converted_path = f"/tmp/{pc_id}.png"
-    image_key = f"{team_id}/{user_id}/{pc_id}.png"
+    image_org_key = f"{team_id}/{user_id}/{pc_id}.png"
 
     response = requests.get(url, stream=True)
     content_type = response.headers["content-type"]
@@ -23,17 +23,100 @@ def write_pc_image(team_id, user_id, pc_id, url):
     with open(image_origin_path, 'wb') as f:
         f.write(response.content)
 
-    image = Image.open(image_origin_path)
-    image.save(image_converted_path)
+    image_org = Image.open(image_origin_path)
+    image_org.resize((400, 400))
+    image_org.save(image_converted_path)
 
     s3_client = boto3.client('s3')
-    s3_client.upload_file(image_converted_path, yig.config.AWS_S3_BUCKET_NAME, image_key)
+    s3_client.upload_file(image_converted_path, yig.config.AWS_S3_BUCKET_NAME, image_org_key)
 
-    response = s3_client.put_object_tagging(
+    s3_client.put_object_tagging(
         Bucket = yig.config.AWS_S3_BUCKET_NAME,
-        Key = image_key,
+        Key = image_org_key,
         Tagging = {'TagSet': [ { 'Key': 'public-object', 'Value': 'yes' }, ]})
-    return image_key
+
+    return image_org_key
+
+def create_param_image(team_id, user_id, pc_id, user_param):
+    # math define
+    n = 8
+    W = H = 400
+    r = 200
+    radian = 2 * math.pi / n;
+    w = h = 0
+    h_o = 5
+
+    # color define
+    white = (255, 255, 255)
+    black = (0, 0, 0)
+    dimgray = (105,105,105)
+    light_sky_blue =  (180,235,250)
+
+    font = ImageFont.truetype("font/04Takibi-Medium.otf", 45)
+    lst_param_name = ["STR", "CON", "POW", "DEX",
+                      "APP", "SIZ", "INT", "EDU"]
+
+
+    canvas = Image.new('RGB', (W, H), white)
+    draw = ImageDraw.Draw(canvas)
+
+    lst_coord =[]
+    for i in range(1, 9):
+        cood = (math.cos(i*radian)*r+W/2,
+                math.sin(i*radian)*r+H/2)
+        lst_coord.append(cood)
+
+    draw.line(lst_coord,
+              fill=black,
+              width=3)
+    params = [float(user_param['POW'])/18,
+              float(user_param['DEX'])/18,
+              float(user_param['APP'])/18,
+              float(user_param['SIZ'])/18,
+              float(user_param['INT'])/18,
+              float(user_param['EDU'])/21,
+              float(user_param['STR'])/18,
+              float(user_param['CON'])/18]
+
+    lst_param_cood = []
+    for i in range(0,7):
+        param_cood = (math.cos(i*radian)*r*params[i]+W/2,
+                      math.sin(i*radian)*r*params[i]+H/2)
+        lst_param_cood.append(param_cood)
+
+    draw.polygon(lst_param_cood,
+                 outline=black,
+                 fill=light_sky_blue)
+
+
+    def get_point(w, h, i):
+        yield [((W-w)/2,h_o), (W-w-h_o,h_o*7),
+               (W-w,W/2-h/2), (W-w-h_o,H-h_o*7-h),
+               ((W-w)/2,H-h-h_o), (h_o,H-h_o*7-h),
+               (0,(H-h)/2), (h_o*1,h_o*7)][i]
+
+    for i, name in enumerate(lst_param_name):
+        w, h = draw.textsize(name, font=font)
+        draw.text(next(get_point(w, h, i)),
+                  name,
+                  dimgray,
+                  font=font)
+
+    return canvas
+
+def save_param_image(image, path, team_id, user_id, pc_id):
+    image_param_path = f"/tmp/{pc_id}_param.png"
+    image_param_key = f"{team_id}/{user_id}/{pc_id}_param.png"
+    image.save(image_param_path)
+
+    s3_client = boto3.client('s3')
+    s3_client.upload_file(image_param_path, yig.config.AWS_S3_BUCKET_NAME, image_param_key)
+    s3_client.put_object_tagging(
+        Bucket = yig.config.AWS_S3_BUCKET_NAME,
+        Key = image_param_key,
+        Tagging = {'TagSet': [ { 'Key': 'public-object', 'Value': 'yes' }, ]})
+
+    return f"https://wheellab-coc-pcparams.s3.ap-northeast-1.amazonaws.com/{team_id}/{user_id}/{pc_id}_param.png"
 
 
 def get_charaimage(team_id, user_id, pc_id):
@@ -63,6 +146,9 @@ def get_pc_icon_url(team_id, user_id, pc_id):
     else:
         return "https://wheellab-coc-pcparams.s3.ap-northeast-1.amazonaws.com/public/noimage.png"
 
+
+def get_param_image_path(team_id, user_id, pc_id):
+    return f"{team_id}/{user_id}/{pc_id}_param.png"
 
 def section_builder(lst_document):
     section_content = []
