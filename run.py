efe9ff25917d56e5b16f719c32db3fa744ece285
cahlchang <kahlua.dane@gmail.com>
2020-09-03 19:45:57 +0900

    local実行用ファイルの再設定（いい感じにしたい）

--- run.py
+++ run.py
@@ -5,7 +5,8 @@ channel_id = 'CNCRFR7KP'
 team_id = 'TE4VDPM2L'
 user_id = 'UE6P69HFH'
 response_url = 'no use'
+channel_name = 'local'
 
-event = {'body': f"text={command}&channel_id={channel_id}&team_id={team_id}&user_id={user_id}&response_url={response_url}"}
+event = {'body': f"text={command}&channel_id={channel_id}&team_id={team_id}&user_id={user_id}&response_url={response_url}&channel_name={channel_name}"}
 
 lambda_function.bootstrap(event, {})
