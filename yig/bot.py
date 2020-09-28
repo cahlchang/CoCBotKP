be3c2ee81d0860ef043fd5924cdac110d31ba2d4
cahlchang <kahlua.dane@gmail.com>
2020-09-28 22:11:09 +0900

    致命的なミス

--- yig/bot.py
+++ yig/bot.py
@@ -63,8 +63,7 @@ class Bot(object):
         print(param_json)
         print(type(param_json))
         logging.info(param_json)
-        print(json.decode(urllib.parse.unquote(contents[-1])))
-        self.team_Id = param_json["user"]["team_id"]
+        self.team_id = param_json["user"]["team_id"]
         self.channel_id = param_json["container"]["channel_id"]
         self.user_id = param_json["user"]["id"]
         self.trigger_id = param_json["trigger_id"]
