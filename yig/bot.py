e9b8df3d29fac1cb3057678e7554d4fb41ac0ef2
cahlchang <kahlua.dane@gmail.com>
2020-10-01 23:27:31 +0900

    idを変更

--- yig/bot.py
+++ yig/bot.py
@@ -62,7 +62,6 @@ class Bot(object):
         param_json = json.loads(urllib.parse.unquote(contents[-1]))
         self.team_id = param_json["user"]["team_id"]
         self.user_id = param_json["user"]["id"]
-        self.view_id = param_json["container"]["view_id"]
         self.trigger_id = param_json["trigger_id"]
         payload = {"token": self.get_token(self.team_id),
                    "user": self.user_id}
--- yig/plugins/gui.py
+++ yig/plugins/gui.py
@@ -85,7 +85,7 @@ def gui_receiver(bot):
     payload = {
         "token": bot.token,
         "channel": bot.channel_id,
-        "view_id": bot.view_id,
+        "view_id": 'danodsad',
         "trigger_id": bot.trigger_id,
         "view": json.dumps(view_content, ensure_ascii=False)
     }
