34a7bde35d916d3c4f2f9207bd003c577fddbe4a
cahlchang <kahlua.dane@gmail.com>
2020-10-01 23:58:41 +0900

    typo

--- yig/bot.py
+++ yig/bot.py
@@ -63,7 +63,7 @@ class Bot(object):
         self.team_id = param_json["user"]["team_id"]
         self.user_id = param_json["user"]["id"]
         self.trigger_id = param_json["trigger_id"]
-        self.view_id = param["view"]["id"]
+        self.view_id = param_json["view"]["id"]
         payload = {"token": self.get_token(self.team_id),
                    "user": self.user_id}
         res = requests.get("https://slack.com/api/users.profile.get",
