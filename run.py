5bb6f5330a27413091537a9a538337b4d4ea9e2c
cahlchang <kahlua.dane@gmail.com>
2021-02-16 05:07:34 +0900

    test channel add

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ channel_id = 'CNCRFR7KP'
 team_id = 'TE4VDPM2L'
 user_id = 'UE6P69HFH'
 response_url = 'no use'
-channel_name = 'local'
+channel_name = 'lab2'
 
 event = {'body': f"text={command}&channel_id={channel_id}&team_id={team_id}&user_id={user_id}&response_url={response_url}&channel_name={channel_name}"}
 
